b3757f08fa806d69cdd47f30c8bc5f5a1f46fa11
hermin9804 <hermin9804@gmail.com>
2024-02-13 00:44:24 +0900

    feat: birth 컬럼 추가

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,5 +2,5 @@ import { UserEntity } from 'src/users/entities/user.entity';
 
 export type CreateUserDto = Pick<
   UserEntity,
-  'snsId' | 'snsAuthProvider' | 'name' | 'email' | 'phoneNumber' | 'gender'
+  'snsId' | 'snsAuthProvider' | 'name' | 'email' | 'phoneNumber' | 'gender' | 'birth'
 >;
