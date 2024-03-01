6558465c6b175d995a72abc6d7856cd688508885
hermin9804 <hermin9804@gmail.com>
2024-03-01 18:28:37 +0900

    refactor: CreateUserDto 수정

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,4 @@
 import { UserEntity } from 'src/users/entities/user.entity';
 
-// 필수 필드를 추출합니다.
-type RequiredFields = Pick<UserEntity, 'snsId' | 'snsAuthProvider' | 'name' | 'email'>;
-
-// 선택적 필드를 추출하고, 모든 필드를 선택적으로 만듭니다.
-type OptionalFields = Partial<Pick<UserEntity, 'phoneNumber' | 'gender' | 'birth'>>;
-
-// 필수 필드와 선택적 필드를 조합합니다.
-export type CreateUserDto = RequiredFields & OptionalFields;
+export type CreateUserDto = Pick<UserEntity, 'snsId' | 'snsAuthProvider' | 'name' | 'email'> &
+  Partial<Pick<UserEntity, 'phoneNumber' | 'gender' | 'birth'>>;
