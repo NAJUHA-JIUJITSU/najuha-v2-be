c988cca469815a7bdaf55a42867290287c79b6d2
hermin9804 <hermin9804@gmail.com>
2024-03-22 15:09:59 +0900

    NJH-302 refactor: dicision unpack

--- src/modules/competitions/domain/division-pack.domain.service.ts
+++ src/modules/competitions/domain/division-pack.domain.service.ts
@@ -7,32 +7,44 @@ import { IDivisionPack } from '../structure/division-pack.interface';
 export class DivisionPackDomainService {
   constructor() {}
 
+  private cartesian(...arrays: any[][]): any[][] {
+    return arrays.reduce((acc, curr) => acc.flatMap((c) => curr.map((n) => [].concat(c, n))), [[]]);
+  }
+
   unpack(id: ICompetition['id'], divisionPack: IDivisionPack): IDivision[] {
-    const divisions: IDivision[] = [];
-    divisionPack.categorys.map((category) => {
-      divisionPack.uniforms.map((uniform) => {
-        divisionPack.genders.map((gender) => {
-          divisionPack.belts.map((belt) => {
-            divisionPack.weights.map((weight) => {
-              const division: IDivision = {
-                competitionId: id,
-                id: 0, // TODO: id를 어떻게 처리할지 고민해보기
-                category,
-                uniform,
-                gender,
-                belt,
-                weight,
-                birthYearRangeStart: divisionPack.birthYearRangeStart,
-                birthYearRangeEnd: divisionPack.birthYearRangeEnd,
-                status: 'ACTIVE',
-                createdAt: new Date(),
-                updatedAt: new Date(),
-              };
-              divisions.push(division);
-            });
-          });
-        });
-      });
+    const combinations = this.cartesian(
+      divisionPack.categorys,
+      divisionPack.uniforms,
+      divisionPack.genders,
+      divisionPack.belts,
+      divisionPack.weights,
+    );
+
+    const divisions: IDivision[] = combinations.map(([category, uniform, gender, belt, weight]) => {
+      const division: IDivision = {
+        competitionId: id,
+        id: 0, // You need to determine how to handle IDs
+        category,
+        uniform,
+        gender,
+        belt,
+        weight,
+        birthYearRangeStart: divisionPack.birthYearRangeStart,
+        birthYearRangeEnd: divisionPack.birthYearRangeEnd,
+        status: 'ACTIVE',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        priceSnapshots: [
+          {
+            id: 0, // Determine ID handling
+            price: divisionPack.price,
+            createdAt: new Date(),
+            divisionId: 0, // This needs to be updated once you have the division ID
+          },
+        ],
+      };
+
+      return division;
     });
     return divisions;
   }
