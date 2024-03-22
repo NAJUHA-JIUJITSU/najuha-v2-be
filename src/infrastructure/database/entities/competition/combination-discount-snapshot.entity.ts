c6ca9d9a4f61e43c5c1321c401ef8bb3006360c6
hermin9804 <hermin9804@gmail.com>
2024-03-23 04:18:16 +0900

    NJH-302 feat: combinationDiscountSnapshotEntity

---
+++ src/infrastructure/database/entities/competition/combination-discount-snapshot.entity.ts
@@ -0,0 +1,36 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { CompetitionEntity } from './competition.entity';
+
+type unit = {
+  uniform: 'GI' | 'NOGI';
+  weight: 'WEIGHT' | 'ABSOLUTE';
+};
+
+type discountRule = {
+  combination: unit[];
+  discountAmount: number;
+};
+
+@Entity('combination_discount_snapshot')
+export class combinationDiscountSnapshotEntity {
+  /**
+   * - combination discount snapshot id.
+   * @type uint32
+   */
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('jsonb')
+  discountRules: discountRule[];
+
+  /** - 생성 시간. 데이터베이스에 엔티티가 처음 저장될 때 자동으로 설정됩니다. */
+  @CreateDateColumn()
+  createdAt: Date | string;
+
+  @Column()
+  competitionId: CompetitionEntity['id'];
+
+  @ManyToOne(() => CompetitionEntity, (competition) => competition.combinationDiscountSnapshots)
+  @JoinColumn({ name: 'competitionId' })
+  competition: CompetitionEntity;
+}
