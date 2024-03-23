0d439df1c5b02b63664ff997b5768bb922b0aef0
hermin9804 <hermin9804@gmail.com>
2024-03-23 15:38:02 +0900

    NJH-302 refactor: typeOmrConfig 분리

--- src/infrastructure/database/typeorm.config.ts
+++ src/infrastructure/database/typeorm.config.ts
@@ -1,16 +1,18 @@
 import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import appEnv from '../../common/app-env';
 
+const typeOrmConfig = {
+  type: appEnv.dbType,
+  host: appEnv.dbHost,
+  port: appEnv.dbPort,
+  username: appEnv.dbUsername,
+  password: appEnv.dbpassword,
+  database: appEnv.dbDatabase,
+  synchronize: appEnv.dbSynchronize,
+  entities: [__dirname + '/entities/**/*.entity{.ts,.js}'],
+  autoLoadEntities: true,
+};
+
 export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
-  useFactory: async (): Promise<TypeOrmModuleOptions> => ({
-    type: appEnv.dbType,
-    host: appEnv.dbHost,
-    port: appEnv.dbPort,
-    username: appEnv.dbUsername,
-    password: appEnv.dbpassword,
-    database: appEnv.dbDatabase,
-    synchronize: appEnv.dbSynchronize,
-    entities: [__dirname + '/entities/**/*.entity{.ts,.js}'],
-    autoLoadEntities: true,
-  }),
+  useFactory: async (): Promise<TypeOrmModuleOptions> => typeOrmConfig,
 };
