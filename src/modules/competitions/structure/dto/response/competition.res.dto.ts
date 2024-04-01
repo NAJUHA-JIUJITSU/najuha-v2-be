081a37fb4943b8513c8128509eea7a4c05bfd662
hermin9804 <hermin9804@gmail.com>
2024-04-01 19:14:46 +0900

    docs: competition 프로퍼티 추가

--- src/modules/competitions/structure/dto/response/competition.res.dto.ts
+++ src/modules/competitions/structure/dto/response/competition.res.dto.ts
@@ -1,4 +1,5 @@
 import { ICombinationDiscountSnapshot } from '../../interface/combination-discount-snapshot.interface';
+import { ICompetition } from '../../interface/competition.interface';
 import { IDivision } from '../../interface/division.interface';
 import { IEarlybirdDiscountSnapshot } from '../../interface/earlybird-discount-snapshot.interface';
 import { IPriceSnapshot } from '../../interface/price-snapshot.interface';
@@ -7,7 +8,7 @@ interface SDivision extends IDivision {
   priceSnapshots: IPriceSnapshot[];
 }
 
-interface SCompetition {
+interface SCompetition extends ICompetition {
   divisions: SDivision[];
   earlybirdDiscountSnapshots: IEarlybirdDiscountSnapshot[];
   combinationDiscountSnapshots: ICombinationDiscountSnapshot[];
