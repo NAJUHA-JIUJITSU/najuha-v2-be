38261181e03e34d6dcb1ff3a5e7fae28cd20672c
hermin9804 <hermin9804@gmail.com>
2024-06-11 20:12:30 +0900

    NJH-400 feat: init public bucket setting

--- src/infrastructure/bucket/bucket.service.ts
+++ src/infrastructure/bucket/bucket.service.ts
@@ -1,6 +1,6 @@
 import appEnv from 'src/common/app-env';
 import { Injectable, Inject, OnModuleInit } from '@nestjs/common';
-import { S3Client, CreateBucketCommand, HeadBucketCommand } from '@aws-sdk/client-s3';
+import { S3Client, CreateBucketCommand, HeadBucketCommand, PutBucketPolicyCommand } from '@aws-sdk/client-s3';
 import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
 import { GetPresignedPostUrlParam, TPresignedPost } from './bucket.interface';
 
@@ -21,13 +21,34 @@ export class BucketService implements OnModuleInit {
       console.log(`Bucket ${this.bucket} exists`);
     } catch (error: any) {
       if (error.name === 'NotFound') {
-        const command = new CreateBucketCommand({ Bucket: this.bucket });
-        await this.client.send(command);
+        const createCommand = new CreateBucketCommand({ Bucket: this.bucket });
+        await this.client.send(createCommand);
         console.log(`Bucket ${this.bucket} created`);
       } else {
         throw error;
       }
     }
+
+    // Set bucket policy to make it public
+    const policy = {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Principal: '*',
+          Action: ['s3:GetObject', 's3:ListBucket'],
+          Resource: [`arn:aws:s3:::${this.bucket}`, `arn:aws:s3:::${this.bucket}/*`],
+        },
+      ],
+    };
+
+    const policyCommand = new PutBucketPolicyCommand({
+      Bucket: this.bucket,
+      Policy: JSON.stringify(policy),
+    });
+
+    await this.client.send(policyCommand);
+    console.log(`Bucket ${this.bucket} is now public`);
   }
 
   async getPresignedPostUrl({
