c08295996d0d4c651bc28e725fae8eb1133ad2d4
hermin9804 <hermin9804@gmail.com>
2024-06-19 16:52:48 +0900

    NJH-400 docs

--- src/modules/images/presentation/user-images.controller.ts
+++ src/modules/images/presentation/user-images.controller.ts
@@ -19,6 +19,16 @@ export class UserImagesController {
    * - presignedPost는 5MB 이하의 이미지만 업로드할 수 있도록 설정되어 있습니다.
    * - imageEntity는 10분 후에도 이미지 소유 Entity에게 FK로 연결되지 않으면 주기적으로 삭제될수 있습니다.
    *
+   * Q. 이미지 소유 Entity에게 FK로 연결된다는게 무슨 의미인가요?
+   * A. 이미지를 bucket에 업로드 이후 imageId 를 u-3-4 createUserProfileImage API를 통해 업로드한 유저에게 FK로 연결합니다.
+   *
+   * Q. 해당 이미지에 어떻게 접근하나요?
+   * A. `${bucketHost}/${bucketName}/${imageEntity.path}/${imageEntity.id}` 형태로 접근할 수 있습니다.
+   * ex) http://localhost:9000/najuha-v2-bucket/competition/01902a06-4db4-71ea-b9a8-2a8a8f489667
+   *
+   * Q. path는 어떻게 결정되나요?
+   * A. u-9-1 createImage 요청시 보내는 body의 path에 따라 결정됩니다. 설정 가능한 path: ['competition', 'user-profile', 'post']
+   *
    * @tag u-9 images
    * @security bearer
    * @param body CreateImageReqBody
